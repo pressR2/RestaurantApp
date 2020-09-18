525514df9d3b7487c5abe93527eacd841f3f30d1
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-09-18 20:59:31 +0200

    feat: settings for github pages

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2hjZWp1emdyYWMiLCJhIjoiY2tmOGxla215MGRkNzJ5cWc3bDlsZTUyaCJ9.sf0GdzXbK47ds5gSXTWkJQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
